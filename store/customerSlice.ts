5cbc956d89fedb76846bbbb406b16c1648a79862
isnael07 <isnaellima24@gmail.com>
2025-07-04 15:40:35 -0300

    feat[]: adiciona o slice de customer

---
+++ store/customerSlice.ts
@@ -0,0 +1,49 @@
+import customer from "@/types/customer";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { randomUUID } from "expo-crypto";
+import { RootState } from "./store";
+
+
+interface custormerState {
+    list: customer[];
+}
+const initialState: custormerState = {
+    list: [],
+};
+
+
+
+const customerSlice =  createSlice({
+    name: 'customer',
+    initialState,
+    reducers: {
+        addCustomer: (state, action: PayloadAction<customer>) => {
+            const customerWithId = {
+                ...action.payload,
+                id: randomUUID()
+            }
+            state.list.push(customerWithId)
+        },
+       setCustomer: (state, action: PayloadAction<customer[]>) => {
+          state.list = action.payload;
+       },
+       updateCustomer: (state, action: PayloadAction<customer>) => {
+         const index = state.list.findIndex(
+            (customer) => customer.id === action.payload.id
+         );
+         if (index !== -1) {
+           state.list[index] = action.payload;
+        }
+       },
+       deleteCustomerById: (state, action: PayloadAction<{id: string}>) => {
+       state.list = state.list.filter(
+        (customer) => customer.id !== action.payload.id
+      );
+    },
+    },
+});
+
+export const findCustomerById = (state: RootState, id: string) => state.customer.list.find((customer) => customer.id === id);
+export const { addCustomer, setCustomer, updateCustomer, deleteCustomerById } = customerSlice.actions;
+
+export default  customerSlice.reducer;
