777859af585a4ef0efd88af196a31bcee099172c
isnael07 <isnaellima24@gmail.com>
2025-07-04 15:41:14 -0300

    feat[]: adiciona o slice de payment

---
+++ store/paymentSlice.ts
@@ -0,0 +1,48 @@
+import payment from "@/types/payment";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { randomUUID } from "expo-crypto";
+import { RootState } from "./store";
+
+
+interface PaymentState {
+    list: payment[];
+}
+const initialState: PaymentState = {
+    list: [],
+};
+
+const paymentSlice =  createSlice({
+    name: 'payment',
+    initialState,
+    reducers: {
+        addPayment: (state, action: PayloadAction<payment>) => {
+            const paymentWithId = {
+                ...action.payload,
+                id: randomUUID()
+            }
+            state.list.push(paymentWithId)
+        },
+       setPayment: (state, action: PayloadAction<payment[]>) => {
+          state.list = action.payload;
+       },
+       updatePayment: (state, action: PayloadAction<payment>) => {
+         const index = state.list.findIndex(
+            (payment) => payment.id === action.payload.id
+         );
+         if (index !== -1) {
+           state.list[index] = action.payload;
+        }
+       },
+       deletePaymentById: (state, action: PayloadAction<{id: string}>) => {
+       state.list = state.list.filter(
+        (payment) => payment.id !== action.payload.id
+      );
+    },
+    },
+});
+
+
+
+export const findPaymentById = (state: RootState, id: string) => state.payment.list.find((payment) => payment.id === id);
+export const { addPayment, setPayment, updatePayment, deletePaymentById } = paymentSlice.actions;
+export default paymentSlice.reducer;
