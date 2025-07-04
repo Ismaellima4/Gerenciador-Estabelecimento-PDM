e0caf5461514fda90bb1a2bfbda5536cb4ec15b0
isnael07 <isnaellima24@gmail.com>
2025-07-04 01:39:34 -0300

    feat[]: adiciona o type de payment

---
+++ types/payment.ts
@@ -0,0 +1,16 @@
+import customer  from "./customer";
+import { PaymentStatus } from "./enum/payment-status.enum";
+import { PaymentType } from "./enum/payment-type.enum";
+import order  from "./order";
+
+type payment = {
+    id: string;
+    order: order;
+    amount: number;
+    date: Date;
+    customer: customer;
+    paymentType: PaymentType;
+    paymentStatus: PaymentStatus;
+}
+
+export default payment;
