8e3c96fda0e6bc77da43e37d70f45e9de2b06b64
isnael07 <isnaellima24@gmail.com>
2025-07-08 19:47:47 -0300

    feat[]: adicina o handle de salvar um customer

--- app/customerRegister.tsx
+++ app/customerRegister.tsx
@@ -1,8 +1,37 @@
 import FormActionButtons from "@/components/FormActionButton";
+import { addCustomer } from "@/store/customerSlice";
 import { registerStyles } from "@/styles/registerStyles";
-import { SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Alert, SafeAreaView, ScrollView, Text, TextInput, View } from "react-native";
+import { useDispatch } from "react-redux";
 
 export default function CustomerRegistration() {
+
+    const [customerName, setCustomerName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+
+    const dispatch = useDispatch();
+
+    const handleSave = () => {
+        if (!customerName || !phone) {
+            Alert.alert("Erro", "Nome e telefone são obrigatórios.");
+            return;
+        }
+        const newCustomer = {
+            name: customerName,
+            cpf,
+            phone,
+            email,
+            payments: [],
+        };
+
+        dispatch(addCustomer(newCustomer));
+        Alert.alert("Sucesso", "Cliente salvo!");
+        router.back();
+    };
 
      return (
       <SafeAreaView style={registerStyles.safeArea}>
@@ -13,6 +42,8 @@ export default function CustomerRegistration() {
               <TextInput
                 style={registerStyles.input}
                 placeholder="Nome do cliente"
+                value={customerName}
+                onChangeText={setCustomerName}
                 placeholderTextColor="#888"
               />
 
@@ -20,12 +51,18 @@ export default function CustomerRegistration() {
               <TextInput
                 style={registerStyles.input}
                 placeholder="CPF"
+                value={cpf}
+                onChangeText={setCpf}
+                keyboardType="numeric"
                 placeholderTextColor="#888"
               />
 
               <Text style={registerStyles.label}>Telefone <Text style={registerStyles.required}>*</Text></Text>
               <TextInput
                 style={registerStyles.input}
+                value={phone}
+                onChangeText={setPhone}
+                maxLength={15}
                 placeholder="(00) 00000-0000"
                 placeholderTextColor="#888"
                 keyboardType="phone-pad"
@@ -35,6 +72,8 @@ export default function CustomerRegistration() {
               <TextInput
                 style={registerStyles.input}
                 placeholder="email@exemplo.com"
+                value={email}
+                onChangeText={setEmail}
                 placeholderTextColor="#888"
                 keyboardType="email-address"
               />
@@ -42,8 +81,8 @@ export default function CustomerRegistration() {
             </View>
 
             <FormActionButtons
-                onSave={() => console.log("Salvar cliente")}
-                onCancel={() => console.log("Cancelar cadastro")}
+                onSave={handleSave}
+                onCancel={() => router.back()}
             />
           </View>
         </ScrollView>
