58a78bff060cddce0c3690a451f7be27a4f7f049
isnael07 <isnaellima24@gmail.com>
2025-07-14 15:29:34 -0300

    feat[]: adiciona o renderItem

--- app/(tabs)/listOrder.tsx
+++ app/(tabs)/listOrder.tsx
@@ -14,7 +14,10 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 
 export default function ListOrders() {
+
+
   const orders = useSelector((state: RootState) => state.order.list);
+  const payments = useSelector((state: RootState) => state.payment.list);
 
   return (
     <SafeAreaView style={listStyles.container}>
@@ -28,36 +31,35 @@ export default function ListOrders() {
       <FlatList
         data={orders}
         keyExtractor={(item, index) => `${item.id}-${index}`}
-        renderItem={({ item }) => (
-          <Link
-            href={{
-              pathname: '/orderDetails',
-              params: { id: item.id },
-            }}
-            asChild
-          >
-            <TouchableOpacity>
-              <View style={listStyles.card}>
-                <Text style={listStyles.cardTitle}>Pedido #{item.id.slice(0, 8)}</Text>
-                <Text style={listStyles.cardInfo}>
-                  Status: {item.orderStatus}
-                </Text>
-                <Text style={listStyles.cardInfo}>
-                  Total: R$ {item.orderItems.reduce((sum, item) => sum + item.product.price * item.quantity, 0).toFixed(2)}
-                </Text>
-                <Text style={listStyles.cardInfo}>
-                  pago: {item.payment?.paymentStatus || 'NÃO PAGO'}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </Link>
-        )}
+        renderItem={({ item }) => {
+          const payment = payments.find((p) => p.order.id === item.id);
+
+          return (
+            <Link
+              href={{
+                pathname: '/orderDetails',
+                params: { id: item.id },
+              }}
+              asChild
+            >
+              <TouchableOpacity>
+                <View style={listStyles.card}>
+                  <Text style={listStyles.cardTitle}>Pedido #{item.id.slice(0, 8)}</Text>
+                  <Text style={listStyles.cardInfo}>Status: {item.orderStatus}</Text>
+                  <Text style={listStyles.cardInfo}>
+                    Total: R$ {item.orderItems.reduce((sum, i) => sum + i.product.price * i.quantity, 0).toFixed(2)}
+                  </Text>
+                  <Text style={listStyles.cardInfo}>
+                    Pagamento: {payment ? payment.paymentType: ''}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </Link>
+          );
+        }}
         ListEmptyComponent={
           <Text style={listStyles.emptyText}>Nenhum pedido cadastrado.</Text>
         }
-
-
-        
       />
     </SafeAreaView>
   );
