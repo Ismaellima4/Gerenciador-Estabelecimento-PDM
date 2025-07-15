43ad1040ada06cb8edaa34cda23800dbe2b2b0ff
ismaellima4 <ismael.github@gmail.com>
2025-07-14 21:07:07 -0300

    feat[]: adiciona tela de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Stack>
+          <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="productDetails" options={{headerTitle: "Detalhes do Produto"}} />
           <Stack.Screen name="suppliersDetails" options={{headerTitle: "Detalhes do Fornecedor"}} />
