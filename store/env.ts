f070900da0754f61a880fa72c0bf4309155e5fc2
isnael07 <isnaellima24@gmail.com>
2025-07-18 23:24:49 -0300

    chore[]: adiciona o const para a rota da api para customers

--- store/env.ts
+++ store/env.ts
@@ -1,7 +1,8 @@
-export const API_URL = 'http://192.168.5.12:3000';
+export const API_URL = 'http://:3000';
 export const API_URL_CATEGORY = `${API_URL}/categories`;
 export const API_URL_SUPPLIER = `${API_URL}/suppliers`;
 export const API_URL_PRODUCT = `${API_URL}/products`;
 export const API_URL_ORDER = `${API_URL}/orders`;
 export const API_URL_PAYMENT = `${API_URL}/payments`;
+export const API_URL_CUSTOMER = `${API_URL}/customers`
 export const API_URL_AUTH_SIGN_IN = `${API_URL}/auth/sign-in`
